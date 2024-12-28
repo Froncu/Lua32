e95e8fa4f591116ef6e26977aa8b87c0ab29505b
Froncu <jakubfratczak204@gmail.com>
2024-12-29 00:55:54 +0100

    [FEATURE] create a separate console window and redirect `stdout` to it in debug mode

--- engine/Game.cpp
+++ engine/Game.cpp
@@ -14,7 +14,22 @@
 
 Game::Game()
 {
-	// nothing to create
+#ifndef NDEBUG
+	if (!AllocConsole())
+		return;
+
+	FILE *fp;
+	freopen_s(&fp, "CONOUT$", "w", stdout);
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+	freopen_s(&fp, "CONOUT$", "w", stderr);
+	setvbuf(stderr, NULL, _IONBF, 0);
+
+	freopen_s(&fp, "CONIN$", "r", stdin);
+	setvbuf(stdin, NULL, _IONBF, 0);
+
+	std::ios::sync_with_stdio(true);
+#endif
 }
 
 Game::~Game()
